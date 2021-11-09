f1f312b4cd4dc7541e785e3fe13df91c290592f8
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-11-09 17:55:11 -0300

    create sidebar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-const Home = () => <div>Hello World</div>;
+import { Container, Search, Logo, Wrapper, Map } from './styles';
+
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do Restaurante" />
+          <TextField
+            outlined
+            label="Pesquisar"
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  );
+};
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+  background-color: ${(props) => props.theme.colors.background};
+  width: 360px;
+  height: 100vh;
+  overflow-y: scroll;
+`;
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 16px;
+`;
+
+export const Logo = styled.img`
+  margin: 15px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const Map = styled.div`
+  background-color: red;
+  width: 100%;
+`;
